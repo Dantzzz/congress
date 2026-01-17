df9244b998591f781611ee4205edfbf4008242b5
Dantz Farrow <dantzfarrow@gmail.com>
2026-01-17 03:05:30 -0500

    Delete temporary practice files; Add PostgreSQL conn test script.

--- json_practice.py
+++
@@ -1,7 +0,0 @@
-import json
-
-api = '{"bill_id": "hr100", "title": "Infrastructure Act", "status": "passed", "votes": {"yea": 245, "nay": 190}}'
-bill_data = json.loads(api)
-
-print(bill_data['title'])
-print(bill_data['votes']['yea'])
